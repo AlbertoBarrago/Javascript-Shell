b06fe6daf8e1d0242f8be048cafe878d6729e1a6
Alberto Barrago <a.barrago@c22.consulting>
2026-07-16 10:05:48 +0200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -11,13 +11,36 @@ const VALID_COMMANDS = ['type', 'echo', 'cd'];
 rl.prompt();
 rl.on('line', (command) => {
   const args = command.trim().split(/\s+/);
-  if (!VALID_COMMANDS.includes(args[0])) {
-    console.log(`${args[0]}: command not found`);
-    rl.prompt();
-    return;
-  } else {
-    console.log(`${args[0]} is a shell builtin`)
+  const commandName = args[0];
+  const commandArgs = args.slice(1);
+  if (commandName === 'type') {
+    if (commandArgs.length === 0) {
+      console.log('type: missing operand');
+      rl.prompt();
+      return;
+    }
+    if (!VALID_COMMANDS.includes(commandArgs[0])) {
+      console.log(`${commandArgs[0]}: command not found`);
+    } else {
+      console.log(`${commandArgs[0]} is a shell builtin`)
+    }
     rl.prompt();
     return;
   }
+  if (!VALID_COMMANDS.includes(commandName)) {
+    console.log(`${commandName}: command not found`);
+    rl.prompt();
+    return;
+  }
+  if (commandName === 'echo') {
+    console.log(commandArgs.join(' '));
+  }
+  if (commandName === 'cd') {
+    if (commandArgs.length === 0) {
+      console.log('cd: missing operand');
+    } else {
+      process.chdir(commandArgs[0]);
+    }
+  }
+  rl.prompt();
 })
